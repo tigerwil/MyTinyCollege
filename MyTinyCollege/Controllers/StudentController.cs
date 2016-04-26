7af3fbfa71946e5cb961f9d3f4b3ee4720ebef17
Marc Williams <oti_instructor@hotmail.com>
2016-04-26 11:59:48 -0300

    Added Overposting check and sorting in student controller

--- MyTinyCollege/Controllers/StudentController.cs
+++ MyTinyCollege/Controllers/StudentController.cs
@@ -16,9 +16,45 @@ namespace MyTinyCollege.Controllers
         private SchoolContext db = new SchoolContext();
 
         // GET: Student
-        public ActionResult Index()
+        //public ActionResult Index()
+        //{
+        //    return View(db.Students.ToList());
+        //}
+
+         //mwilliams:  adding sorting functionality
+        public ActionResult Index(string sortOrder)
         {
-            return View(db.Students.ToList());
+
+            //Prepare sort order 
+            ViewBag.CurrentSort = sortOrder;//get current sort from UI
+            ViewBag.FNameSortParm = string.IsNullOrEmpty(sortOrder) ? "fname_desc" : "";
+            ViewBag.LNameSortParm = string.IsNullOrEmpty(sortOrder) ? "lname_desc" : "";
+            //ViewBag.DateSortParm = sortOrder
+
+            //Let's get our student data
+            var students = from s in db.Students select s;
+
+            //Apply the sort order 
+            switch (sortOrder)
+            {
+                //LastName Desc
+                case "lname_desc":
+                    students = students.OrderByDescending(s => s.LastName);
+                    break;
+
+                //FirstName Desc
+                case "fname_desc":
+                    students = students.OrderByDescending(s => s.FirstName);
+                    break;
+                
+                //Default LastName Asc
+                default:
+                    students = students.OrderBy(s => s.LastName);
+                    break;
+            }
+            //return the students object as a enumerable (list)
+            return View(students.ToList());
+
         }
 
         // GET: Student/Details/5
@@ -86,26 +122,57 @@ namespace MyTinyCollege.Controllers
         // POST: Student/Edit/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
+        [HttpPost, ActionName("Edit")]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ID,LastName,FirstName,Email,EnrollmentDate")] Student student)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(student).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(student);
-        }
-
-        // GET: Student/Delete/5
-        public ActionResult Delete(int? id)
+        public ActionResult EditPost(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
+
+            var studentToUpdate = db.Students.Find(id);
+            if(TryUpdateModel(studentToUpdate,"",
+                new string[] { "LastName", "FirstName", "EnrollmentDate", "Email" }))
+            {
+                try
+                {
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                catch (Exception)
+                {
+                    ModelState.AddModelError("", "Unable to save changes. Try again later!");
+                }
+            }
+            //Irregardless of the outcome (success or fail) we return the student model
+            //with edit view or Index view
+            return View(studentToUpdate);
+        }
+        //public ActionResult Edit([Bind(Include = "ID,LastName,FirstName,Email,EnrollmentDate")] Student student)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        db.Entry(student).State = EntityState.Modified;
+        //        db.SaveChanges();
+        //        return RedirectToAction("Index");
+        //    }
+        //    return View(student);
+        //}
+
+        // GET: Student/Delete/5
+        public ActionResult Delete(int? id, bool? saveChangesError=false)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            //check for error
+            if (saveChangesError.GetValueOrDefault())
+            {
+                ViewBag.ErrorMessage = "Delete failed please try again.";
+            }
+
             Student student = db.Students.Find(id);
             if (student == null)
             {
@@ -115,13 +182,23 @@ namespace MyTinyCollege.Controllers
         }
 
         // POST: Student/Delete/5
-        [HttpPost, ActionName("Delete")]
+        [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
+        public ActionResult Delete(int id)
         {
-            Student student = db.Students.Find(id);
-            db.People.Remove(student);
-            db.SaveChanges();
+            try
+            {
+                Student student = db.Students.Find(id);
+                db.People.Remove(student);
+                db.SaveChanges();
+            }
+            catch (Exception)
+            {
+                //redirect user back to get Delete with same item (id)
+                //including a boolean flag parameter stating that an error has occured
+                return RedirectToAction("Delete", new { id = id, saveChangesError = true });
+            }
+
             return RedirectToAction("Index");
         }
 
