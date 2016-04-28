3477895bad51b615b074ab94eae556c00a681b47
Marc Williams <oti_instructor@hotmail.com>
2016-04-28 10:10:35 -0300

    Reading related data

---
+++ MyTinyCollege/Controllers/InstructorController.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MyTinyCollege.DAL;
+using MyTinyCollege.Models;
+using MyTinyCollege.ViewModels;
+
+namespace MyTinyCollege.Controllers
+{
+    public class InstructorController : Controller
+    {
+        private SchoolContext db = new SchoolContext();
+
+        // GET: Instructor
+        public ActionResult Index(int? id, int? courseID)
+        {
+            //int? id -> for determining which instructor was selected ->load related courses
+            //int> courseID ->  for determining which course was selected->load related students
+           
+            var viewModel = new InstructorIndexData();
+
+
+            //Eager loading
+            viewModel.Instructors = db.Instructors
+                .Include(i => i.OfficeAssignment)
+                .Include(i => i.Courses)
+                .OrderBy(i => i.LastName);
+
+            //check for id (an instructor has been selected -> get courses that instructor teaches)
+            if (id != null)
+            {
+                ViewBag.InstructorID = id.Value;//for UI:  which instructor row is selected
+                //Lazy Loading
+                viewModel.Courses = viewModel.Instructors
+                                    .Where(i => i.ID == id.Value).Single().Courses;
+
+            }
+
+            //check for courseID (a course has been selected -> get enrolled students
+            if (courseID != null)
+            {
+                //Lazy Loading
+                //viewModel.Enrollments = viewModel.Courses
+                //                        .Where(x => x.CourseID == courseID.Value)
+                //                        .Single().Enrollments;
+
+                
+                var selectedCourse = viewModel.Courses
+                                        .Where(x => x.CourseID == courseID.Value)
+                                        .Single();
+
+                //Explicity Loading
+                db.Entry(selectedCourse).Collection(x => x.Enrollments).Load();
+                foreach (Enrollment enrollment in selectedCourse.Enrollments)
+                {
+                    db.Entry(enrollment).Reference(x => x.student).Load();
+                }
+                viewModel.Enrollments = selectedCourse.Enrollments;
+
+            }
+
+            //return the view attaching the viewModel (instructors, courses, enrollments)
+            return View(viewModel);
+
+
+        }
+
+        // GET: Instructor/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Instructor instructor = db.Instructors.Find(id);
+            if (instructor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(instructor);
+        }
+
+        // GET: Instructor/Create
+        public ActionResult Create()
+        {
+            ViewBag.ID = new SelectList(db.OfficeAssignments, "InstructorID", "Location");
+            return View();
+        }
+
+        // POST: Instructor/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,LastName,FirstName,Email,HireDate")] Instructor instructor)
+        {
+            if (ModelState.IsValid)
+            {
+                db.People.Add(instructor);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ID = new SelectList(db.OfficeAssignments, "InstructorID", "Location", instructor.ID);
+            return View(instructor);
+        }
+
+        // GET: Instructor/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Instructor instructor = db.Instructors.Find(id);
+            if (instructor == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ID = new SelectList(db.OfficeAssignments, "InstructorID", "Location", instructor.ID);
+            return View(instructor);
+        }
+
+        // POST: Instructor/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,LastName,FirstName,Email,HireDate")] Instructor instructor)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(instructor).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ID = new SelectList(db.OfficeAssignments, "InstructorID", "Location", instructor.ID);
+            return View(instructor);
+        }
+
+        // GET: Instructor/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Instructor instructor = db.Instructors.Find(id);
+            if (instructor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(instructor);
+        }
+
+        // POST: Instructor/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Instructor instructor = db.Instructors.Find(id);
+            db.People.Remove(instructor);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MyTinyCollege/ViewModels/InstructorIndexData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MyTinyCollege.Models;
+
+namespace MyTinyCollege.ViewModels
+{
+    public class InstructorIndexData
+    {
+        //For Reading Related Data:  Instructors - Courses - Enrollment
+        public IEnumerable<Instructor> Instructors { get; set; }
+        public IEnumerable<Course> Courses { get; set; }
+        public IEnumerable<Enrollment> Enrollments { get; set; }
+    }
+}
