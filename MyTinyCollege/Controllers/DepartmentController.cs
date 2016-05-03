45888fdf458a71de59634a5afd12aa8aa4c93590
Marc Williams <oti_instructor@hotmail.com>
2016-05-03 09:42:28 -0300

    Department image upload complete

--- MyTinyCollege/Controllers/DepartmentController.cs
+++ MyTinyCollege/Controllers/DepartmentController.cs
@@ -9,6 +9,7 @@ using System.Web;
 using System.Web.Mvc;
 using MyTinyCollege.DAL;
 using MyTinyCollege.Models;
+using MyTinyCollege.Helpers;
 
 namespace MyTinyCollege.Controllers
 {
@@ -60,9 +61,43 @@ namespace MyTinyCollege.Controllers
                 //Note the HttpPostedFileBase ImageName args in method
                 //Also the form needs a enctype="multipart/form-data"
                 // And <input type="file" id="ImageName" name="ImageName" accept="image/*" class="form-control" />
-                db.Departments.Add(department);
-                await db.SaveChangesAsync();
-                return RedirectToAction("Index");
+
+                if(ImageName !=null && ImageName.ContentLength > 0)
+                {
+                    //do we have anything to upload - yes
+                    var validImageTypes = new string[]
+                    {
+                        //"image/gif",
+                        //"image/jpg",
+                        //"image/jpeg",
+                        "image/png"
+                    };
+                    if (!validImageTypes.Contains(ImageName.ContentType))
+                    {
+                        //file being uploaded is not a png -display error
+                        ModelState.AddModelError("", "Please choose a PNG image.");
+                        return View(department);
+
+                    }
+                    //save new department to database
+                    db.Departments.Add(department);
+                    await db.SaveChangesAsync();
+                    //Retrieve the IDENTITY from SQL Server
+                    string pictureName = department.DepartmentID.ToString();
+
+                    //Rename, scale and upload image
+                    ImageUpload imageUpload = new ImageUpload { Width = 128 };
+                    ImageResult imageResult = imageUpload.RenameUploadFile(ImageName, pictureName);
+
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    //nothing to upload - display error
+                    ModelState.AddModelError("", "You have not selected an image file to upload.");
+                    return View(department);
+                }
+  
             }
 
             
