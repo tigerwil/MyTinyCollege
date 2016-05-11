aeb4f997b4732fd7972d8d9de83794f1cfc7f8fe
Marc Williams <oti_instructor@hotmail.com>
2016-05-11 10:15:08 -0300

    Ajax Autocomplete Search

--- MyTinyCollege/Controllers/HomeController.cs
+++ MyTinyCollege/Controllers/HomeController.cs
@@ -42,5 +42,32 @@ namespace MyTinyCollege.Controllers
             //return Content("<h1>This is just a test</h1>");
         }
 
+        //mwilliams:  Ajax Search Methods
+        private List<Models.Instructor> GetFaculty(string searchString)
+        {
+            return db.Instructors
+               .Where(i => i.LastName.Contains(searchString) || 
+               i.FirstName.Contains(searchString)).ToList();
+        }
+
+        //This is to display a list of Faculty
+        public ActionResult FacultySearch(string q)
+        {
+            //get search results
+            var faculty = GetFaculty(q);
+            return PartialView("_FacultySearch", faculty);
+        }
+
+        //This is the AJAX - Autocomplete
+        public ActionResult QuickSearch(string term)
+        {
+            var faculty = GetFaculty(term)
+                .Select(a => new { value = a.FirstName });
+            return Json(faculty, JsonRequestBehavior.AllowGet);
+        }
+
+
+        //mwilliams:  end Ajax Search Methods
+
     }
 }
---
+++ MyTinyCollege/Scripts/App/CollegeScripts.js
@@ -0,0 +1,11 @@
+﻿$(function () {
+    $("[data-autocomplete-source]").each(function () {
+        var target = $(this);
+        target.autocomplete({ source: target.attr("data-autocomplete-source") });
+    });
+});
+
+function searchFailed() {
+    $("#searchresults").html("Sorry, there was problem with the search!")
+}
+
